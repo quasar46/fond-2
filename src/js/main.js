9f3143948b08041733f0503688b6d694285eeb8b
lock <falsivates@gmail.com>
2021-03-24 13:26:49 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,30 @@ $('.sliderAdvantages').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
 });
+
+$('.toggle').click(function (event) {
+    event.preventDefault();
+    $('.burgerMenu').slideToggle();
+});
+
+
+$(document).ready(function () {
+    $.fn.equivalent = function () {
+        var $blocks = $('.itemWrapperFace__subtitle'),
+            maxH = $blocks.height();
+        $blocks.each(function () {
+            maxH = ($('.itemWrapperFace__subtitle').height() > maxH) ? $('.itemWrapperFace__subtitle').height() : maxH;
+        });
+        $blocks.height(maxH);
+    };
+    $('.itemWrapperFace--book').equivalent();
+});
+
+$('.fotodateSlick').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    customPaging: function (slick, index) {
+        return '<a>' + (index + 1) + '</a>';
+    }
+});
